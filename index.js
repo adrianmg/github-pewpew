2ab465b49e40140955650df191507cb331f58302
Adrian Mato <adrianmg@github.com>
2021-08-06 22:05:11 -0700

    Add: check for PAT permissions

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const childProcess = require('child_process');
-const { prompt } = require('enquirer');
 const { promisify } = require('util');
+const { prompt } = require('enquirer');
+const style = require('ansi-colors');
 const utils = require('./src/utils');
 
 const exec = promisify(childProcess.exec);
@@ -10,21 +11,23 @@ const exec = promisify(childProcess.exec);
 (async function main() {
   utils.printWelcome();
 
-  const username = (await exec('git config user.email')).stdout.trim();
-
+  const gitUser = (await exec('git config user.email')).stdout.trim();
+  console.log(`GitHub login:`);
   const res = await prompt([
     {
       type: 'input',
-      name: 'name',
+      name: 'username',
       message: 'Username',
-      default: username,
+      default: gitUser,
     },
     {
       type: 'password',
       name: 'pat',
-      message: 'Personal access token (PAT)',
+      message: `Personal Access Token`,
     },
   ]);
+
+  await utils.checkPermissions(res.username, res.pat);
 })().catch((err) => {
   console.error(err);
 });
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
-const style = require('ansi-colors');
+const childProcess = require('child_process');
+const { promisify } = require('util');
 const package = require('../package.json');
+const style = require('ansi-colors');
+const ora = require('ora');
+
+const exec = promisify(childProcess.exec);
 
 function printWelcome() {
   const name = package.name;
@@ -7,14 +12,34 @@ function printWelcome() {
   const version = package.version;
 
   if (name && description && version) {
-    console.log(
-      `Welcome to ${style.greenBright(`${name} v${version}`)} — ${description}!`
-    );
-    // console.log(description);
+    console.log(`Welcome to ${style.greenBright(`${name} v${version}`)}`);
+    console.log(description);
     console.log();
   }
 }
 
+async function checkPermissions(username, pat) {
+  let spinner = ora('Checking permissions…');
+  spinner.start();
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1500);
+  });
+
+  const curl = `curl --head -u ${username}:${pat} https://api.github.com/users/adrianmg`;
+  const { stdout } = await exec(curl);
+
+  if (!stdout.includes('delete_repo')) {
+    spinner.fail(
+      `Oops! Check your account details. You can generate a PAT (scoped to 'delete_repo') on https://github.com/settings/tokens`
+    );
+    process.exit(0);
+  }
+
+  spinner.succeed(`Permissions OK`);
+  return true;
+}
+
 module.exports = {
   printWelcome,
+  checkPermissions,
 };
