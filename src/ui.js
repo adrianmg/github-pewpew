8701386604e0577f7735a4ca73c2c212b27a91ab
Adrian Mato <adrianmg@github.com>
2021-08-16 19:54:57 -0700

    Better order based on logic

--- src/ui.js
+++ src/ui.js
@@ -15,6 +15,42 @@ function printWelcome() {
   }
 }
 
+async function promptGetRepositories(repositories) {
+  return await prompt({
+    type: 'autocomplete',
+    name: 'repos',
+    message: 'Select repositories you want to delete:',
+    limit: 12,
+    multiple: true,
+    footer: '––—————––—————––—————––—————––————————————',
+    format: (value) => style.green(value),
+    choices: repositories.map(({ full_name }) => full_name),
+  });
+}
+
+async function promptConfirmDelete(repoCount) {
+  return await prompt({
+    type: 'select',
+    name: 'confirmDelete',
+    message: `Are you sure?`,
+    format: (value) => value,
+    choices: [
+      {
+        name: 'Yes',
+        message: `${style.redBright(
+          `Yes, delete ${repoCount > 1 ? 'repositories' : 'repository'} (${repoCount})`
+        )}`,
+        value: true,
+      },
+      {
+        name: 'Cancel',
+        message: 'Cancel',
+        value: false,
+      },
+    ],
+  });
+}
+
 function printConfirmDelete(deletedRepos) {
   const strConfirm = `🔫 pew pew! ${deletedRepos} repositories deleted suscessfully.`;
   const strRecover = `Recover repositories from github.com/settings/repositories`;
@@ -41,48 +77,12 @@ function printError(strError) {
   return console.log(style.redBright(strError));
 }
 
-async function promptConfirmDelete(repoCount) {
-  return await prompt({
-    type: 'select',
-    name: 'confirmDelete',
-    message: `Are you sure?`,
-    format: (value) => value,
-    choices: [
-      {
-        name: 'Yes',
-        message: `${style.redBright(
-          `Yes, delete ${repoCount > 1 ? 'repositories' : 'repository'} (${repoCount})`
-        )}`,
-        value: true,
-      },
-      {
-        name: 'Cancel',
-        message: 'Cancel',
-        value: false,
-      },
-    ],
-  });
-}
-
-async function promptGetRepositories(repositories) {
-  return await prompt({
-    type: 'autocomplete',
-    name: 'repos',
-    message: 'Select repositories you want to delete:',
-    limit: 12,
-    multiple: true,
-    footer: '––—————––—————––—————––—————––————————————',
-    format: (value) => style.green(value),
-    choices: repositories.map(({ full_name }) => full_name),
-  });
-}
-
 module.exports = {
   printWelcome,
+  promptGetRepositories,
+  promptConfirmDelete,
   printConfirmDelete,
   printNoReposDeleted,
   printNoReposSelected,
   printError,
-  promptConfirmDelete,
-  promptGetRepositories,
 };
