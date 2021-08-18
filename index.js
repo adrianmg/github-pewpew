2baa1eff421fd91d1c7ee69860544f0b2121b4e4
Adrian Mato <adrianmg@github.com>
2021-08-17 23:50:07 -0700

    Trying out a new main() structure

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 #!/usr/bin/env node
 const { auth, getRepositories, deleteRepository } = require('./src/github');
-const { loadConfig, saveConfig } = require('./src/config');
+const { loadConfig, saveConfig, deleteConfig } = require('./src/config');
 const UI = require('./src/ui');
 
-(async function main() {
-  UI.printWelcome();
+UI.printWelcome();
 
-  if (!loadConfig()) {
-    const token = await auth();
-    await saveConfig(token);
-  }
+main();
 
-  const repositories = await getRepositories();
-  let res = await UI.promptGetRepositories(repositories);
-
-  if (res.repos.length === 0) {
-    UI.printNoReposSelected();
-    process.exit();
-  }
-
-  const reposToDelete = res.repos;
-  const repoCount = reposToDelete.length;
-  res = await UI.promptConfirmDelete(repoCount);
-
-  if (res.confirmDelete === 'Yes') {
-    let deletedRepos = 0;
-    for (const repo of reposToDelete) {
-      const status = await deleteRepository(repo);
-      if (status) deletedRepos++;
+async function main() {
+  try {
+    if (!loadConfig()) {
+      const token = await auth();
+      await saveConfig(token);
     }
-    UI.printConfirmDelete(deletedRepos);
-  } else {
-    UI.printNoReposDeleted();
+
+    const repositories = await getRepositories();
+    if (!repositories) {
+      deleteConfig();
+      console.log('hi from deleteConfig');
+      main();
+    }
+
+    let res = await UI.promptSelectRepositories(repositories);
+    if (res.repos?.length === 0) {
+      UI.printNoReposSelected();
+      process.exit(0);
+    }
+
+    const reposToDelete = res.repos;
+    const repoCount = reposToDelete.length;
+    res = await UI.promptConfirmDelete(repoCount);
+
+    if (res.confirmDelete === 'Yes') {
+      let deletedRepos = 0;
+      for (const repo of reposToDelete) {
+        const status = await deleteRepository(repo);
+        if (status) deletedRepos++;
+      }
+      UI.printConfirmDelete(deletedRepos);
+    } else {
+      UI.printNoReposDeleted();
+    }
+  } catch (error) {
+    // TODO: fix entering in error when user cancels the command before finishing it
+    // is it due to an unsolved promise in any of the prompts?
+    UI.printError(error);
+
+    console.log('Hi from catch');
+    deleteConfig();
+    main();
   }
-})().catch((err) => {
-  UI.printError(err);
-});
+}
