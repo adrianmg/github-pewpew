45a923807346b90322ac4ef82c3ddf14f7454a66
jdvr <juandavidvegarguez@gmail.com>
2022-09-29 18:07:27 +0200

    use `.config` as default config dir

--- src/config.js
+++ src/config.js
@@ -8,6 +8,12 @@ const { package: PACKAGE, author: PACKAGE_AUTHOR } = Utils.getPackageDetails();
 const HOME_DIR = require('os').homedir();
 const CONFIG_DIR = getConfigDir(HOME_DIR);
 const CONFIG_FILE = path.join(CONFIG_DIR, 'auth.json');
+const CONFIG_PATH_BY_PLATFORM = {
+  'win32': path.join('AppData', 'Roaming', PACKAGE_AUTHOR, PACKAGE.name),
+  'darwin': path.join('Library', `com.${PACKAGE_AUTHOR}.${PACKAGE.name}`)
+}
+const DEFAULT_CONFIG_PATH = path.join('.config', `com.${PACKAGE_AUTHOR}.${PACKAGE.name}`)
+
 
 function save(token) {
   const configuration = {
@@ -41,11 +47,10 @@ function deleteFile() {
 }
 
 function getConfigDir(homeDir) {
+  const configPath = CONFIG_PATH_BY_PLATFORM[process.platform] || DEFAULT_CONFIG_ROOT
   const configDir = path.join(
     homeDir,
-    process.platform === 'win32'
-      ? path.join('AppData', 'Roaming', PACKAGE_AUTHOR, PACKAGE.name)
-      : path.join('Library', `com.${PACKAGE_AUTHOR}.${PACKAGE.name}`)
+    configPath
   );
 
   return configDir;
