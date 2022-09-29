648103d62cc9d7de22f3952f4bcd664269734e28
jdvr <juandavidvegarguez@gmail.com>
2022-09-29 18:16:52 +0200

    fix variable order

--- src/config.js
+++ src/config.js
@@ -6,14 +6,14 @@ const Utils = require('./utils');
 
 const { package: PACKAGE, author: PACKAGE_AUTHOR } = Utils.getPackageDetails();
 const HOME_DIR = require('os').homedir();
-const CONFIG_DIR = getConfigDir(HOME_DIR);
-const CONFIG_FILE = path.join(CONFIG_DIR, 'auth.json');
+
 const CONFIG_PATH_BY_PLATFORM = {
   'win32': path.join('AppData', 'Roaming', PACKAGE_AUTHOR, PACKAGE.name),
   'darwin': path.join('Library', `com.${PACKAGE_AUTHOR}.${PACKAGE.name}`)
 }
 const DEFAULT_CONFIG_PATH = path.join('.config', `com.${PACKAGE_AUTHOR}.${PACKAGE.name}`)
-
+const CONFIG_DIR = getConfigDir(HOME_DIR);
+const CONFIG_FILE = path.join(CONFIG_DIR, 'auth.json');
 
 function save(token) {
   const configuration = {
@@ -47,7 +47,7 @@ function deleteFile() {
 }
 
 function getConfigDir(homeDir) {
-  const configPath = CONFIG_PATH_BY_PLATFORM[process.platform] || DEFAULT_CONFIG_ROOT
+  const configPath = CONFIG_PATH_BY_PLATFORM[process.platform] || DEFAULT_CONFIG_PATH
   const configDir = path.join(
     homeDir,
     configPath
